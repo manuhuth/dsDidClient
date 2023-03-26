3bbec5bc0c6d0f620b8a9b2f0ff62a84b91d710a
carolinalvarez <carolina.alvarezg21@gmail.com>
2023-03-26 21:33:42 +0200

    trying to separate error from current code

--- tests/testthat.R
+++ tests/testthat.R
@@ -14,6 +14,7 @@ install.packages('DSLite', repos = "http://cran.us.r-project.org")
 install.packages('devtools', repos = "http://cran.us.r-project.org")
 library(devtools)
 devtools::install_github("manuhuth/dsDid")
+devtools::install_github("manuhuth/dsDidClient") #
 devtools::install_github("datashield/dsBase")
 devtools::install_github("datashield/dsBaseClient")
 devtools::install_github("datashield/DSI")
--- tests/testthat/test-ds.R
+++ tests/testthat/test-ds.R
@@ -12,23 +12,23 @@ test_that("ds_test_basic_set_up_check", {
    dsBaseClient::ds.mean(x="D$X", datasources = connections)
 })
 
-test_that("ds_run_doubly_robust", {
-
-  data_object <- create_test_data(seed=12345)
-  connections <- data_object$datasources
-  data <- data_object$datasources
-
-  our_federated_package <- ds.did(yname="Y", tname="period", idname="id", gname="G",
-                                  t_periods = c(2, 3), g_periods=c(2, 3),
-                                  data="D", #xformla="X",
-                                  control_group= "notyettreated",
-                                  anticipation=0,  alpha=0.05,
-                                  base_period = "varying",
-                                  bstrap=FALSE, biters = 1000, cband = TRUE,
-                                  clustervars = NULL,
-                                  est_method="dr",
-                                  datasources = connections,
-                                  clear_console=TRUE)
-print(DSI::datashield.errors())
-
-})
+# test_that("ds_run_doubly_robust", {
+#
+#   data_object <- create_test_data(seed=12345)
+#   connections <- data_object$datasources
+#   data <- data_object$datasources
+#
+#   our_federated_package <- ds.did(yname="Y", tname="period", idname="id", gname="G",
+#                                   t_periods = c(2, 3), g_periods=c(2, 3),
+#                                   data="D", #xformla="X",
+#                                   control_group= "notyettreated",
+#                                   anticipation=0,  alpha=0.05,
+#                                   base_period = "varying",
+#                                   bstrap=FALSE, biters = 1000, cband = TRUE,
+#                                   clustervars = NULL,
+#                                   est_method="dr",
+#                                   datasources = connections,
+#                                   clear_console=TRUE)
+# print(DSI::datashield.errors())
+#
+# })
