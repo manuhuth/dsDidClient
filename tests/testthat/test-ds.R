09b3641d3c10ef6b86cb04f536890b9c4c9f0663
manuhuth <s6mahuth@uni-bonn.de>
2023-03-31 11:44:42 +0200

    Add tests on ipw, reg, universal base periods, and never treated individuals as control groups.

--- tests/testthat/test-ds.R
+++ tests/testthat/test-ds.R
@@ -36,6 +36,54 @@ test_that("ds_run_doubly_robust", {
 
 })
 
+test_that("ds_run_ipw", {
+
+  data_object <- create_test_data(seed=1235)
+  connections <- connections
+  data <- data_object$datasources
+
+  our_federated_package <- ds.did(yname="Y", tname="period", idname="id", gname="G",
+                                  t_periods = c(2, 3), g_periods=c(2, 3),
+                                  data="D", #xformla="X",
+                                  control_group= "notyettreated",
+                                  anticipation=0,  alpha=0.05,
+                                  base_period = "universal",
+                                  bstrap=FALSE, biters = 1000, cband = TRUE,
+                                  clustervars = NULL,
+                                  est_method="ipw",
+                                  datasources = connections,
+                                  clear_console=TRUE)
+
+
+
+  expect_no_error(our_federated_package)
+
+})
+
+test_that("ds_run_doubly_robust_nevertreated", {
+
+  data_object <- create_test_data(seed=1235)
+  connections <- connections
+  data <- data_object$datasources
+
+  our_federated_package <- ds.did(yname="Y", tname="period", idname="id", gname="G",
+                                  t_periods = c(2, 3), g_periods=c(2, 3),
+                                  data="D", #xformla="X",
+                                  control_group= "nevertreated",
+                                  anticipation=0,  alpha=0.05,
+                                  base_period = "varying",
+                                  bstrap=FALSE, biters = 1000, cband = TRUE,
+                                  clustervars = NULL,
+                                  est_method="reg",
+                                  datasources = connections,
+                                  clear_console=TRUE)
+
+
+
+  expect_no_error(our_federated_package)
+
+})
+
 test_that("ds_run_doubly_robust_covs", {
 
   data_object <- create_test_data(seed=1235)
@@ -43,7 +91,8 @@ test_that("ds_run_doubly_robust_covs", {
   data <- data_object$datasources
 
   #expect warning is necessary since ds.glm throws warning at every step
-  #of the gradient descent; algoprithms converge after 3rd step
+  #(calls glm for every step)
+  #of the gradient descent; but actually the algoprithms converge after the 3rd step
   #thiss needs to be fixed in the base package
   suppressWarnings(our_federated_package <- ds.did(yname="Y", tname="period", idname="id", gname="G",
                                   t_periods = c(2, 3), g_periods=c(2, 3),
